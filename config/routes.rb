80a0966a372b729d69875f1951c3df0af80509c8
Moises Zaragoza <mZaragoza@circletechfl.com>
2014-12-29 16:17:21 -0500

    set layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,22 @@
 Rails.application.routes.draw do
-  devise_for :admins
+   devise_for :admins, :controllers => {
+    :sessions => "admins/sessions",
+    :passwords => 'admins/passwords',
+    :confirmations => 'admins/confirmations'
+  }
+
+  authenticate :admin do
+    namespace :admins do
+      resources :admins
+      resources :profile, :only => [:edit, :update]
+      resources :users
+      get '/dashboard' => 'dashboards#index', as: :dashboard
+      get '/' => 'dashboards#index'
+      root 'dashboards#index'
+    end
+  end
+
+
   match 'ping'         => 'tools#ping',           as: :ping, via: :all
   root 'pages#index'
 end
